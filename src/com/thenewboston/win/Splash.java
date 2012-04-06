39af3f1e1e71eac592594ced2c0ec1919a785b13
Winston Nolan <winston.nolan@gmail.com>
2012-04-06 10:32:43 +0200

    Initial Comit

---
+++ src/com/thenewboston/win/Splash.java
@@ -0,0 +1,49 @@
+package com.thenewboston.win;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+	MediaPlayer ourSong;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		
+		ourSong = MediaPlayer.create(Splash.this, R.raw.whatsupdoc);
+		ourSong.start();
+		
+		Thread timer = new Thread() {
+			@Override
+			public void run() {
+				try {
+					sleep(5000);
+				}
+				catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				finally {
+					Intent openStartingPoint = new Intent("com.thenewboston.win.MENU");
+					startActivity(openStartingPoint);
+				}
+			}
+		};
+		timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		ourSong.release();
+		finish();
+	}
+	
+	
+
+}
